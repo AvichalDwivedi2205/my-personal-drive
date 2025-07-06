b36a25e61df197d19f8276d3367d770dc9224d50
Avichal <avichaldwivedi2005@gmail.com>
2025-07-06 15:03:50 +0000

    Database Cnfiguration Added

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,15 @@ import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
+  dialect: "singlestore",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    host: env.SINGLESTORE_HOST,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
   },
   tablesFilter: ["drive-tutorial_*"],
+  // tablesFilter is used to filter the tables that are used in the database with the prefix mentioned in the schema
 } satisfies Config;
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,5 +1,6 @@
 import { createClient, type Client } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
+import { createPool, type Pool } from "mysql2/promise";
 
 import { env } from "~/env";
 import * as schema from "./schema";
@@ -9,11 +10,30 @@ import * as schema from "./schema";
  * update.
  */
 const globalForDb = globalThis as unknown as {
-  client: Client | undefined;
+   conn: Pool | undefined;
 };
+// What this does is that it creates a global object that is used to store the database connection
+// This is used to avoid creating a new connection on every HMR update.
 
-export const client =
-  globalForDb.client ?? createClient({ url: env.DATABASE_URL });
-if (env.NODE_ENV !== "production") globalForDb.client = client;
+const conn = globalForDb.conn ?? 
+createPool({
+  host: env.SINGLESTORE_HOST,
+  port: parseInt(env.SINGLESTORE_PORT),
+  user: env.SINGLESTORE_USER,
+  password: env.SINGLESTORE_PASS,
+  database: env.SINGLESTORE_DB_NAME,
+  ssl: {},
+  maxIdle: 0
+});
 
-export const db = drizzle(client, { schema });
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+
+conn.addListener("error", (err) => {
+  console.error("Database connection error:", err);
+});
+
+
+// Drizzle config does not work with pools
+// So we are using mysql2/promise to create a pooled connection
+// And then we are using drizzle to connect to the database
+// And then we are using drizzle to connect to the database
