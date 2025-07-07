2cc9b4052a9d45bed70df6c5cba27489cadcac84
Avichal <avichaldwivedi2005@gmail.com>
2025-07-07 13:15:31 +0000

    Breakcrumbs fixed

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ## Todo
 
 - [X] Set up database and data models
-- [ ] Move folder open state to URL
+- [X] Move folder open state to URL
 - [ ] Add Auth
 - [ ] Add file uploading
--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -4,8 +4,24 @@ import {
   folders as foldersSchema  
 } from "~/server/db/schema";
 import DriveContents from "../../drive-contents";
-import { z } from "zod";
 import { eq } from "drizzle-orm";
+
+async function getAllParents(folderId:number){
+    const parents = [];
+    let currentId: number | null = folderId;
+    while(currentId!==null){
+        const folder = await db
+            .select()
+            .from(foldersSchema)
+            .where(eq(foldersSchema.id, currentId));
+        if(!folder[0]){
+            throw new Error("Folder not found");
+        }
+        parents.unshift(folder[0]);
+        currentId = folder[0].parent;
+    }
+    return parents;
+}
 
 export default async function GoogleDriveClone(props:{
     params: Promise<{ folderId : string }>
@@ -17,9 +33,12 @@ export default async function GoogleDriveClone(props:{
     return <div>Invalid folder ID</div>;
   }
 
-  const files = await db.select().from(filesSchema).where(eq(filesSchema.parent, parsedFolderId));
-  const folders = await db.select().from(foldersSchema).where(eq(foldersSchema.parent, parsedFolderId));
-  return <DriveContents files={files} folders={folders} />
+  const parentsPromise = getAllParents(parsedFolderId);
+
+  const filesPromise =  db.select().from(filesSchema).where(eq(filesSchema.parent, parsedFolderId));
+  const foldersPromise = db.select().from(foldersSchema).where(eq(foldersSchema.parent, parsedFolderId));
+  const [files, folders, parents] = await Promise.all([filesPromise, foldersPromise, parentsPromise]);
+  return <DriveContents files={files} folders={folders} parents={parents} />
 }
 
 // This page cannot be static because folderId is a dynamic parameter
